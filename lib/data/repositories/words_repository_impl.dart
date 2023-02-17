7b1966a0ea7dc47d20fe49f86adff5f5708fc03e
alex <khorunzhiyal@gmail.com>
2023-02-17 20:26:41 +0200

    n36 Word lists (backend)

--- lib/data/repositories/words_repository_impl.dart
+++ lib/data/repositories/words_repository_impl.dart
@@ -1,8 +1,10 @@
 import 'dart:collection';
 
+import 'package:flutter/widgets.dart';
 import 'package:injectable/injectable.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:voca/data/repositories/assets_repository_impl.dart';
 import 'package:voca/data/utils/pos_map.dart';
 import 'package:voca/domain/entities/dictionary_entry.dart';
 import 'package:voca/domain/entities/word.dart';
@@ -13,24 +15,30 @@ import 'package:voca/domain/repositories/words_repository.dart';
 
 @LazySingleton(as: WordsRepository)
 class WordsRepositoryImpl implements WordsRepository {
-  Future<Database>? _db;
+  Database? _db;
   Future<Database> get db async {
-    _db ??= openDatabase(
-      join(await getDatabasesPath(), 'en_dictionary.db'),
-      readOnly: true,
-    );
+    if (_db == null) {
+      debugPrint('initializing db');
+
+      _db = await openDatabase(
+        join(await getDatabasesPath(), AssetsRepositoryImpl.enDictionaryDbName),
+      );
+
+      await _detachAllDb(_db!);
+      await _attachUserProgressDb(_db!);
+    }
 
     return _db!;
   }
 
   @override
-  Future<List<WordCardShort>> findWords(String word) async {
+  Future<List<WordCardShort>> findWords(String query) async {
     final db = await this.db;
 
     final qWords = await db.rawQuery('''
       SELECT rowid, word FROM word WHERE word LIKE ?
     ''', [
-      '$word%',
+      '$query%',
     ]);
 
     final words = <WordCardShort>[];
@@ -100,5 +108,78 @@ class WordsRepositoryImpl implements WordsRepository {
         status: WordCardStatus.learningOrLearned,
       ),
     );
+  }
+
+  @override
+  Future<void> addWordToKnownList(Word word) async {
+    final db = await this.db;
+
+    await db.execute('''
+      INSERT INTO known(wordId, word)
+      VALUES (?, ?)
+    ''', [
+      word.id,
+      word.name,
+    ]);
+  }
+
+  @override
+  Future<void> addWordToLearnList(Word word) async {
+    final db = await this.db;
+
+    await db.execute('''
+      INSERT INTO up.learning(wordId, word, lastRepetition) 
+      VALUES (?, ?, ?)
+    ''', [
+      word.id,
+      word.name,
+      DateTime.now().millisecondsSinceEpoch,
+    ]);
+  }
+
+  /// For hot reload - sqflite connection persists even after hotreload, so I
+  /// get an error when attaching another database
+  Future<void> _detachAllDb(Database mainConnection) async {
+    final dbs = await mainConnection.rawQuery('''
+      SELECT name FROM pragma_database_list
+      WHERE name <> 'main'
+    ''');
+
+    for (final row in dbs) {
+      final name = row['name'];
+      mainConnection.execute('DETACH ?', [name]);
+    }
+  }
+
+  Future<void> _attachUserProgressDb(Database mainConnection) async {
+    final upPath = join(
+      await getDatabasesPath(),
+      'en_user_progress.db',
+    );
+
+    // wordId - references words in the dictionary db; **not a primary key**.
+    final updb = await openDatabase(
+      upPath,
+      version: 1,
+      onCreate: (db, version) {
+        db.execute('''
+          CREATE TABLE learning (
+            wordId INTEGER UNIQUE NOT NULL,
+            word TEXT, 
+            repetitions INTEGER DEFAULT 0,
+            lastRepetition INTEGER
+          )
+        ''');
+        db.execute('''
+          CREATE TABLE known (
+            wordId INTEGER UNIQUE NOT NULL,
+            word TEXT
+          )
+        ''');
+      },
+    );
+    await updb.close();
+
+    await mainConnection.execute('ATTACH ? AS up', [upPath]);
   }
 }
--- lib/domain/repositories/words_repository.dart
+++ lib/domain/repositories/words_repository.dart
@@ -3,6 +3,9 @@ import 'package:voca/domain/entities/word_card.dart';
 import 'package:voca/domain/entities/word_card_short.dart';
 
 abstract class WordsRepository {
-  Future<List<WordCardShort>> findWords(String word);
+  Future<List<WordCardShort>> findWords(String query);
   Future<WordCard> fetchWordCard(Word word);
+
+  Future<void> addWordToLearnList(Word word);
+  Future<void> addWordToKnownList(Word word);
 }
