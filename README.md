73008ea58b263c1666d791bc5ac69e081447bd88
alex <khorunzhiyal@gmail.com>
2023-03-20 23:58:38 +0200

    n58 About (WordNet citation)

--- README.md
+++ README.md
@@ -11,6 +11,6 @@ flutter pub run build_runner build --delete-conflicting-outputs
 ```
 
 ## Sources
-Wordnet: Princeton University "About WordNet." WordNet. Princeton University. 2010. https://wordnet.princeton.edu/
+Wordnet: https://wordnet.princeton.edu/
 
 Words by frequency of use dataset: https://www.kaggle.com/datasets/wheelercode/dictionary-word-frequency?resource=download
---
+++ lib/presentation/settings/widgets/about_banner.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:voca/presentation/base/base_theme.dart';
+import 'package:voca/presentation/base/l10n/gen/strings.g.dart';
+import 'package:voca/presentation/base/widgets/base_card.dart';
+
+final _wordNetUrl = Uri.parse('https://wordnet.princeton.edu/');
+
+class AboutBanner extends StatelessWidget {
+  const AboutBanner({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final t = Translations.of(context);
+
+    return BaseCard(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            t.settings.about.header,
+            textAlign: TextAlign.left,
+            style: const TextStyle(
+              fontSize: 19,
+              fontWeight: FontWeights.bold,
+            ),
+          ),
+          bulidWordNetCitation(),
+        ],
+      ),
+    );
+  }
+
+  Wrap bulidWordNetCitation() {
+    return Wrap(
+      crossAxisAlignment: WrapCrossAlignment.center,
+      children: [
+        for (final word in (t.settings.about.dictionaryFrom).split(' '))
+          Text(
+            '$word ',
+            style: const TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeights.regular,
+            ),
+          ),
+        OutlinedButton.icon(
+          onPressed: () async {
+            await launchUrl(
+              _wordNetUrl,
+              mode: LaunchMode.externalApplication,
+            );
+          },
+          style: const ButtonStyle(
+            padding: MaterialStatePropertyAll(
+                EdgeInsets.symmetric(vertical: 2, horizontal: 4)),
+            textStyle: MaterialStatePropertyAll(
+              TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeights.regular,
+              ),
+            ),
+          ),
+          icon: const Icon(Icons.link),
+          label: Text(t.settings.about.wordNet),
+        ),
+      ],
+    );
+  }
+}
